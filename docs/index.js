c207130c9147bc925738d4450fe399d108158a81
[Agnes Font] <[afonttorres@gmail.com]>
2022-05-03 12:46:11 +0200

    modal added, searcher filter fixed & zoom disabled

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,33 @@
-//falta crear un modal a app i fer-lo servir al toDo i al searcher
 const app = {
-    capitalize: function capitalizeFirstLetter(string) {
+    closeModal() {
+        if (this.mainContainer.children.length > 1) {
+            this.mainContainer.removeChild(document.getElementsByClassName('alert-opacity')[0]);
+            document.querySelectorAll('input')[0].focus();
+        } else {
+            console.log('No modal to close')
+        }
+    },
+    modal(msg) {
+        this.closeModal();
+        let alert = `<div class="alert-opacity">
+                            <div class="alert-container">
+                                <span id="msg">${msg}</span>
+                                <div id="modal-button-container" class="alert-button-row">
+                                    <button type="button" onclick="app.closeModal()">OK</button>
+                                </div>
+                            </div>
+                        </div>`
+        document.querySelectorAll('input')[0].blur();
+        if (this.mainContainer.children.length === 1) {
+            this.mainContainer.insertAdjacentHTML('beforeend', alert);
+        } else {
+            this.mainContainer.removeChild(document.getElementsByClassName('alert-opacity')[0]);
+            setTimeout(() => {
+                this.mainContainer.insertAdjacentHTML('beforeend', alert);
+            }, 50)
+        }
+    },
+    capitalize(string) {
         let capitalizedArr = [];
         for (let i = 0; i < string.length; i++) {
             let letter = string[i];
@@ -11,18 +38,18 @@ const app = {
         }
         return capitalizedArr.toString().split(',').join('');
     },
-    focus: function sendInputValEnter() {
+    focus() {
         let toDoInput = document.getElementById('todo-input');
         toDoInput.focus();
         toDoInput.onkeyup = (e) => {
             if (e.key === 'Enter') toDo.save();
         }
     },
-    setIndex: function setIndex(id, arr) {
+    setIndex(id, arr) {
         let index = arr.findIndex(arrItem => arrItem.id === id);
         return index
     },
-    findInput: function findInput(id) {
+    findInput(id) {
         let inputs = document.getElementsByClassName('update-input');
         for (input of inputs) {
             let inputId = input.id;
@@ -35,7 +62,7 @@ const app = {
         }
     },
     mainContainer: document.getElementsByClassName('wrapper')[0],
-    init: function init() {
+    init() {
         this.focus();
     }
 }
@@ -44,7 +71,7 @@ const toDo = {
     list: [],
     renderListFlag: false,
     listContainer: document.getElementById('list'),
-    save: function saveToDo() {
+    save() {
         let input = document.getElementById('todo-input');
         let inputVal = input.value;
 
@@ -65,13 +92,13 @@ const toDo = {
             this.list.push(todo);
             console.log('Task added')
         } else {
-            console.log('Wrong type of')
+            app.modal(`Impossible to add "${inputVal}", wrong input text`)
         }
 
         input.value = "";
         this.render(this.list);
     },
-    create: function createToDo(element) {
+    create(element) {
 
         let task = app.capitalize(element.task)
         let taskOverflowWrap;
@@ -99,7 +126,7 @@ const toDo = {
 
         this.listContainer.innerHTML += item
     },
-    render: function renderList(arrToRender) {
+    render(arrToRender) {
         this.renderListFlag = true;
         searcher.checkSearcherRender(arrToRender);
         this.listContainer.innerHTML = '';
@@ -108,20 +135,20 @@ const toDo = {
             document.getElementsByClassName('clear-all')[0].style.display = 'block';
         else document.getElementsByClassName('clear-all')[0].style.display = 'none';
     },
-    clearAll: function clearAll() {
+    clearAll() {
         this.list = [];
         this.render(this.list);
     },
-    erase: function eraseItem(id) {
+    erase(id) {
         let index = app.setIndex(id, this.list)
         setTimeout(() => {
             this.list.splice(index, 1);
-            setTimeout(this.askConfirmation(id), this.changeModal(`Task#${id} erased!`), 50);
+            setTimeout(app.modal(`Task#${id} erased!`), 50);
             this.render(this.list);
             console.log('Task erased')
         }, 50);
     },
-    updateStatus: function changeStatus(id) {
+    updateStatus(id) {
         let index = app.setIndex(id, toDo.list)
         let statusDisplay = document.getElementsByClassName('status');
         let selDisplay;
@@ -141,59 +168,37 @@ const toDo = {
         }
 
     },
-    closeModal: function closeModal() {
-        app.mainContainer.removeChild(document.getElementsByClassName('alert-opacity')[0]);
-        document.querySelectorAll('input')[0].focus();
-    },
-    changeModal: function changeModalMsg(msg) {
-        document.getElementById('msg').innerText = msg;
-        document.getElementsByClassName('alert-button-row')[0].innerHTML = `<button type="button" onclick="toDo.closeModal()">OK</button>`;
-    },
-    changeTaskValue: function changeValue(id) {
+    changeTaskValue(id) {
         let input = app.findInput(id);
         let index = app.setIndex(id, toDo.list);
         let inputVal = input.value;
         if (typeof inputVal === 'string' && inputVal !== '') {
             this.list[index]['task'] = inputVal;
             input.value = '';
-            this.closeModal();
-            setTimeout(this.askConfirmation(id), this.changeModal(`Task#${id} updated!`), 50);
+            app.closeModal();
+            setTimeout(this.askConfirmation(id), app.modal(`Task#${id} updated!`), 50);
             this.render(this.list);
             console.log('Task updated')
         } else {
-            this.changeModal(`Impossible to update task#${id}, wrong input text!`);
+            app.modal(`Impossible to update task#${id}, wrong input text!`);
         }
     },
-    cancelUpdate: function cancelUpdate(id) {
+    cancelUpdate(id) {
         let input = app.findInput(id);
         if (input) {
-            this.closeModal();
+            app.closeModal();
             this.render(this.list);
         } else {
             console.log('Something went wrong')
         }
     },
-    askConfirmation: function askConfirmation(id) {
-        let alert = `<div class="alert-opacity">
-                        <div class="alert-container">
-                            <span id="msg">Are you sure you want to change your taks? </span>
-                            <div class="alert-button-row">
-                                <button type="button" onclick="toDo.changeTaskValue(${id})">Yes</button>
-                                <button type="button" onclick="toDo.cancelUpdate(${id})">Cancel</button>
-                            </div>
-                        </div>
-                    </div>`
-        if (app.mainContainer.children.length === 1) {
-            app.mainContainer.insertAdjacentHTML('beforeend', alert);
-        } else {
-            app.mainContainer.removeChild(document.getElementsByClassName('alert-opacity')[0]);
-            setTimeout(() => {
-                app.mainContainer.insertAdjacentHTML('beforeend', alert);
-            }, 50)
-        }
-
+    askConfirmation(id) {
+        app.modal(`Are you sure you want to change taks#${id}?`);
+        document.getElementById('modal-button-container').innerHTML =
+            `<button type="button" onclick="toDo.changeTaskValue(${id})">Yes</button>
+        <button type="button" onclick="toDo.cancelUpdate(${id})">Cancel</button>`
     },
-    enableUpdate: function enableUpdate(id) {
+    enableUpdate(id) {
         let input = app.findInput(id);
         input.value = "";
         input.onkeyup = (e) => {
@@ -201,7 +206,7 @@ const toDo = {
         }
         input.parentElement.parentElement.children[2].innerHTML = `<i class="fa-solid fa-check check-icon" onclick="toDo.askConfirmation(${id})"></i>`;
     },
-    transformTask: function enableTask(task, id) {
+    transformTask(task, id) {
         taskContainer = task.parentElement;
         let oldTask = task.innerHTML;
         taskContainer.innerHTML = `<input id="update-input-${id}" class="update-input" type="text" value='${oldTask}' placeholder="${oldTask}" autocomplete="off">`;
@@ -209,7 +214,7 @@ const toDo = {
         updateInput.focus();
         updateInput.onfocus = this.enableUpdate(id)
     },
-    update: function updateItem(id) {
+    update(id) {
         let inputs = document.getElementsByClassName('update-input');
         for (let input of inputs) {
             if (input) {
@@ -225,7 +230,7 @@ const toDo = {
 }
 
 const searcher = {
-    render: function renderSearcher() {
+    render() {
         let searcherContainer = app.mainContainer.getElementsByClassName('searcher-container')[0];
         document.getElementsByClassName('input-container')[0].style.visibility = 'hidden';
         document.getElementsByClassName('clear-all')[0].style.display = 'none';
@@ -238,7 +243,7 @@ const searcher = {
         document.getElementsByClassName('searcher-input')[0].focus();
 
     },
-    hide: function hideSearcher() {
+    hide() {
         let searcherContainer = app.mainContainer.getElementsByClassName('searcher-container')[0];
         searcherContainer.innerHTML = `<p class="search-button" onclick="searcher.render()"><i class="fa-solid fa-magnifying-glass"></i> Search your task!</p>`
         document.getElementsByClassName('main-title')[0].innerText = 'TO DO LIST'
@@ -247,28 +252,20 @@ const searcher = {
         document.getElementsByClassName('clear-all')[0].style.display = 'none';
         toDo.render(toDo.list);
     },
-    checkSearcherRender: function checkSearcher(arrToRender) {
+    checkSearcherRender(arrToRender) {
         if (arrToRender === toDo.list) {
             let closeSearcherButton = document.getElementsByClassName('searcher-close-button')[0];
             if (closeSearcherButton) this.hide();
         } else return true
     },
-    find: function findSuggestions(data) {
-        let suggestions = [];
-        suggestions = toDo.list.filter((todo) => {
-            if (data.length === 1) {
-                if (todo.task[0] === data) return todo;
-            } else {
-                let cut = data.length;
-                let cuttedTask = todo.task.slice(0, cut);
-                if (cuttedTask === data) return todo;
-            }
-        });
-        if (suggestions.length === 0 && data) setTimeout(toDo.askConfirmation(-1), toDo.changeModal(`Impossible to find ${data}`, 50)); //falta crear un modal i canviarli el valor en funcio
+    find(data) {
+        let cut = data.length;
+        let suggestions = toDo.list.filter(todo => todo.task.slice(0, cut) === data);
+        if (suggestions.length === 0 && data) setTimeout(app.modal(`Impossible to find ${data}`), 50); //falta crear un modal i canviarli el valor en funcio
         toDo.render(suggestions);
 
     },
-    getData: function getUserData(e) {
+    getData(e) {
         let userData = e.target.value;
         if (e.target.value) {
             document.getElementsByClassName('main-title')[0].innerText = 'SEARCHING YOUR TO DO...'
@@ -276,7 +273,7 @@ const searcher = {
             this.find(userData.toLowerCase());
         }
     },
-    search: function search() {
+    search() {
         let searcherInput = document.getElementsByClassName('searcher-input')[0];
         searcherInput.onkeyup = (e) => {
             searcherInput.classList.add('focused-input');
